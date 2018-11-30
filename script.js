b2893f33968d540fcf73e93c0fc16e73c82c0357
Phantom <pharaon.abakan@gmail.com>
2018-11-30 16:38:42 +0700

    git status!

--- script.js
+++ script.js
@@ -1,84 +1,127 @@
 'use strict';
 
-let num = 50;
+// function calc(a,b) {
+//     return (a + b);
+// }
 
-(num == 50) ? console.log("Yes!"): console.log("No!");
+let calc = (a,b) => a+b;
 
-let num1 = 30;
+console.log(calc(3,4));
 
-switch (num1) {
-    case 30:
-        console.log("Yes!");
-        break;
-    default:
-        console.log("WTF?");
-        break;
-}
+console.log(calc(8,4));
 
-let numb = 30;
+let str = "test";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
 
-while (numb < 40) {
-    console.log(numb);
-    numb++;
-}
-
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-    continue;
-    }
-    console.log(i)
-}
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++, decr--);
-console.log(incr, decr);
-console.log(45 % 7);
-console.log(4 === 4);
-
-let isChecked = true,
-    isClosed = false;
-
-console.log(isChecked || !isClosed);
-
-console.log(isChecked && !isClosed);
-
-console.log(4 + "roll");
+let twelve = "12.2px";
+// console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
 
 
-let number = 777;
 
-console.log(number);
 
-let string1 = "SUPER";
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
 
-console.log(string1);
+// let anotherNum = retVar();
 
-let boolean = true;
+// console.log(anotherNum);
 
-console.log(boolean);
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
 
-console.log(persone.name, persone.age, persone.isMarried);
 
-let arr = ['a.png', 'b.png', 'c.png', 'd.png'];
 
-console.log(arr[3]);
+// function showFirstMessage(text)
+// {
+//     alert(text);
+// }
 
-let string = true;
+// showFirstMessage("Hello World!");
 
-console.log(string);
+// let num = 50;
 
-alert("ПРИВЕТ!");
+// (num == 50) ? console.log("Yes!"): console.log("No!");
 
-let answer1 = confirm("YES?");
-console.log(answer1);
+// let num1 = 30;
 
-let answer2 = +prompt("Who You Are?", "Man");
-console.log(typeof (answer2));
+// switch (num1) {
+//     case 30:
+//         console.log("Yes!");
+//         break;
+//     default:
+//         console.log("WTF?");
+//         break;
+// }
+
+// let numb = 30;
+
+// while (numb < 40) {
+//     console.log(numb);
+//     numb++;
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//     continue;
+//     }
+//     console.log(i)
+// }
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++, decr--);
+// console.log(incr, decr);
+// console.log(45 % 7);
+// console.log(4 === 4);
+
+// let isChecked = true,
+//     isClosed = false;
+
+// console.log(isChecked || !isClosed);
+
+// console.log(isChecked && !isClosed);
+
+// console.log(4 + "roll");
+
+
+// let number = 777;
+
+// console.log(number);
+
+// let string1 = "SUPER";
+
+// console.log(string1);
+
+// let boolean = true;
+
+// console.log(boolean);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone.name, persone.age, persone.isMarried);
+
+// let arr = ['a.png', 'b.png', 'c.png', 'd.png'];
+
+// console.log(arr[3]);
+
+// let string = true;
+
+// console.log(string);
+
+// alert("ПРИВЕТ!");
+
+// let answer1 = confirm("YES?");
+// console.log(answer1);
+
+// let answer2 = +prompt("Who You Are?", "Man");
+// console.log(typeof (answer2));
